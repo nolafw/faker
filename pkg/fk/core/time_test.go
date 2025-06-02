eb62592e92a446b379187a0b6627d2bb218f625c
ichikawa <68413973+yrichika@users.noreply.github.com>
2025-06-03 08:08:15 +0900

    Refactor tests: update time_test.go to use Ginkgo and add missing test cases for duration and time functions

--- pkg/fk/core/time_test.go
+++ pkg/fk/core/time_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/nolafw/faker/pkg/fk/common/util"
 	"github.com/nolafw/faker/pkg/fk/core"
+	"github.com/nolafw/faker/pkg/fk/testutil"
 )
 
 var _ = Describe("Tests for random time functions", func() {
@@ -24,28 +25,104 @@ var _ = Describe("Tests for random time functions", func() {
 			Expect(r).To(BeTemporally("<=", future30Years))
 		})
 	})
-	// TODO:
-	Describe("PastFrom", func() {})
 
-	Describe("Past", func() {})
+	Describe("PastFrom", func() {
+		It("should return any random past time from the given time", func() {
+			r := randTime.PastFrom(past30Years)
 
-	Describe("FutureTo", func() {})
+			Expect(r).To(BeTemporally(">=", past30Years))
+			Expect(r).To(BeTemporally("<=", time.Now()))
+		})
+	})
 
-	Describe("Future", func() {})
+	Describe("Past", func() {
+		It("should return any random past time", func() {
+			r := randTime.Past()
 
-	Describe("TimeRange", func() {})
+			Expect(r).To(BeTemporally(">=", past30Years))
+		})
+	})
 
-	Describe("Duration", func() {})
+	Describe("FutureTo", func() {
+		It("should return any random future time to the given time", func() {
+			r := randTime.FutureTo(future30Years)
 
-	Describe("DurationMilliSec", func() {})
+			Expect(r).To(BeTemporally(">=", time.Now()))
+			Expect(r).To(BeTemporally("<=", future30Years))
+		})
+	})
 
-	Describe("DurationSec", func() {})
+	Describe("Future", func() {
+		It("should return any random future time", func() {
+			r := randTime.Future()
 
-	Describe("DurationMin", func() {})
+			Expect(r).To(BeTemporally("<=", future30Years))
+		})
+	})
 
-	Describe("DurationHour", func() {})
+	Describe("TimeRange", func() {
+		It("should return any random time in the given range", func() {
+			r := randTime.TimeRange(past30Years, future30Years)
 
-	Describe("DurationTo", func() {})
+			Expect(r).To(BeTemporally(">=", past30Years))
+			Expect(r).To(BeTemporally("<=", future30Years))
+		})
+	})
 
-	Describe("DurationRange", func() {})
+	Describe("Duration", func() {
+		It("should return any random duration", func() {
+			r := randTime.Duration()
+
+			testutil.Output("RandTime.Duration", r)
+		})
+	})
+
+	Describe("DurationMilliSec", func() {
+		It("should return any random duration in milliseconds", func() {
+			r := randTime.DurationMilliSec()
+
+			testutil.Output("RandTime.DurationMilliSec", r)
+		})
+	})
+
+	Describe("DurationSec", func() {
+		It("should return any random duration in seconds", func() {
+			r := randTime.DurationSec()
+
+			testutil.Output("RandTime.DurationSec", r)
+		})
+	})
+
+	Describe("DurationMin", func() {
+		It("should return any random duration in minutes", func() {
+			r := randTime.DurationMin()
+
+			testutil.Output("RandTime.DurationMin", r)
+		})
+	})
+
+	Describe("DurationHour", func() {
+		It("should return any random duration in hours", func() {
+			r := randTime.DurationHour()
+
+			testutil.Output("RandTime.DurationHour", r)
+		})
+	})
+
+	Describe("DurationTo", func() {
+		It("should return any random duration to the given time", func() {
+			r := randTime.DurationTo(1 * time.Second)
+
+			Expect(r).To(BeNumerically("<=", 1*time.Second))
+		})
+	})
+
+	Describe("DurationRange", func() {
+		It("should return any random duration in the given range", func() {
+			r := randTime.DurationRange(1*time.Second, 2*time.Second)
+
+			Expect(r).To(BeNumerically(">=", 1*time.Second))
+			Expect(r).To(BeNumerically("<=", 2*time.Second))
+		})
+	})
 })
