b973f56034abdedb9a879323d53a9c3d8cddaf74
ichikawa <68413973+yrichika@users.noreply.github.com>
2025-06-03 17:57:00 +0900

    Refactor tests: migrate address tests to Ginkgo and create address_suite_test.go; update BuildingNames variable in ja_JP addresses

--- pkg/fk/generator/address/address_test.go
+++ pkg/fk/generator/address/address_test.go
@@ -1,233 +1,220 @@
-package address
+package address_test
 
 import (
-	"testing"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 
 	"github.com/nolafw/faker/pkg/fk/common/util"
 	"github.com/nolafw/faker/pkg/fk/core"
+	"github.com/nolafw/faker/pkg/fk/generator/address"
 	"github.com/nolafw/faker/pkg/fk/provider/locale/en_US"
 	"github.com/nolafw/faker/pkg/fk/provider/locale/ja_JP"
 	"github.com/nolafw/faker/pkg/fk/testutil"
-	"github.com/yrichika/gest/pkg/gt"
 )
 
-func TestAddress(testingT *testing.T) {
+var _ = Describe("Address", func() {
 	localized := en_US.New()
 	coreRand := core.NewRand(util.RandSeed())
-	address := New(coreRand, localized)
+	addressUs := address.New(coreRand, localized)
 
-	tC := gt.CreateTest(testingT)
-	tC.Describe("Country", func() {
-		tC.It("should return a country", func() {
-			r := address.Country()
-			gt.Expect(tC, &r).ToBeIn(en_US.Countries)
+	Describe("Country", func() {
+		It("should return a country", func() {
+			r := addressUs.Country()
+			Expect(r).To(BeElementOf(en_US.Countries))
 		})
 	})
 
-	tP := gt.CreateTest(testingT)
-	tP.Describe("Postcode", func() {
-		tP.It("should return a postcode", func() {
-			r := address.Postcode()
+	Describe("Postcode", func() {
+		It("should return a postcode", func() {
+			r := addressUs.Postcode()
 			testutil.Output("Address.Postcode", r)
 		})
 	})
 
-	tStateAbbr := gt.CreateTest(testingT)
-	tStateAbbr.Describe("StateAbbr", func() {
-		tStateAbbr.It("should return a state abbreviation", func() {
-			r := address.StateAbbr()
-			gt.Expect(tStateAbbr, &r).ToBeIn(en_US.StateAbbrs)
+	Describe("StateAbbr", func() {
+		It("should return a state abbreviation", func() {
+			r := addressUs.StateAbbr()
+			Expect(r).To(BeElementOf(en_US.StateAbbrs))
 		})
 	})
 
-	tState := gt.CreateTest(testingT)
-	tState.Describe("State", func() {
-		tState.It("should return a state", func() {
-			r := address.State()
-			gt.Expect(tState, &r).ToBeIn(en_US.States)
+	Describe("State", func() {
+		It("should return a state", func() {
+			r := addressUs.State()
+			Expect(r).To(BeElementOf(en_US.States))
 		})
 	})
 
-	tCity := gt.CreateTest(testingT)
-	tCity.Describe("City", func() {
-		tCity.Test("CityName should return a city name", func() {
-			r := address.CityName()
-			gt.Expect(tCity, &r).ToBeIn(*en_US.CityNames)
+	Describe("City", func() {
+		It("CityName should return a city name", func() {
+			r := addressUs.CityName()
+			Expect(r).To(BeElementOf(*en_US.CityNames))
 		})
 
-		tCity.Test("CitySuffix should return a city suffix", func() {
-			r := address.CitySuffix()
-			gt.Expect(tCity, &r).ToBeIn(en_US.CitySuffixes)
+		It("CitySuffix should return a city suffix", func() {
+			r := addressUs.CitySuffix()
+			Expect(r).To(BeElementOf(en_US.CitySuffixes))
 		})
 
-		tCity.Test("CityPrefix should return a city prefix", func() {
-			r := address.CityPrefix()
-			gt.Expect(tCity, &r).ToBeIn(en_US.CityPrefixes)
+		It("CityPrefix should return a city prefix", func() {
+			r := addressUs.CityPrefix()
+			Expect(r).To(BeElementOf(en_US.CityPrefixes))
 		})
 
-		tCity.Test("City should return a city", func() {
-			r := address.City()
+		It("City should return a city", func() {
+			r := addressUs.City()
 			testutil.Output("Address.City", r)
 		})
 	})
 
-	tStreet := gt.CreateTest(testingT)
-	tStreet.Describe("Street", func() {
-		tStreet.Test("StreetName should return a street name", func() {
-			r := address.StreetName()
-			gt.Expect(tStreet, &r).ToBeIn(*en_US.StreetNames)
+	Describe("Street", func() {
+		It("StreetName should return a street name", func() {
+			r := addressUs.StreetName()
+			Expect(r).To(BeElementOf(*en_US.StreetNames))
 		})
 
-		tStreet.Test("StreetSuffix should return a street suffix", func() {
-			r := address.StreetSuffix()
-			gt.Expect(tStreet, &r).ToBeIn(en_US.StreetSuffixes)
+		It("StreetSuffix should return a street suffix", func() {
+			r := addressUs.StreetSuffix()
+			Expect(r).To(BeElementOf(en_US.StreetSuffixes))
 		})
 
-		tStreet.Test("Street should return a street name", func() {
-			r := address.Street()
+		It("Street should return a street name", func() {
+			r := addressUs.Street()
 			testutil.Output("Address.Street", r)
 		})
 	})
 
-	tSec := gt.CreateTest(testingT)
-	tSec.Describe("SecondaryAddress", func() {
-		tSec.Test("BuildingNumber should return a building number", func() {
-			r := address.BuildingNumber()
+	Describe("SecondaryAddress", func() {
+		It("BuildingNumber should return a building number", func() {
+			r := addressUs.BuildingNumber()
 			testutil.Output("Address.BuildingNumber", r)
 		})
 
-		tSec.Test("SecondaryAddress should return a secondary address", func() {
-			r := address.SecondaryAddress()
+		It("SecondaryAddress should return a secondary address", func() {
+			r := addressUs.SecondaryAddress()
 			testutil.Output("Address.SecondaryAddress", r)
 		})
 	})
 
-	tStAddress := gt.CreateTest(testingT)
-	tStAddress.Describe("StreetAddress", func() {
-		tStAddress.It("should return a street address", func() {
-			r := address.StreetAddress()
+	Describe("StreetAddress", func() {
+		It("should return a street address", func() {
+			r := addressUs.StreetAddress()
 			testutil.Output("Address.StreetAddress", r)
 		})
 	})
 
-	tAddr := gt.CreateTest(testingT)
-	tAddr.Describe("Address", func() {
-		tAddr.It("should return an address", func() {
-			r := address.Address()
+	Describe("Address", func() {
+		It("should return an address", func() {
+			r := addressUs.Address()
 			testutil.Output("Address.Address", r)
 		})
 	})
 
-	tLat := gt.CreateTest(testingT)
-	tLat.Describe("Latitude", func() {
-		tLat.It("should return a latitude", func() {
-			r := address.Latitude()
-
-			gt.Expect(tLat, &r).ToBe_(gt.Between(-90.0, 90.0))
+	Describe("Latitude", func() {
+		It("should return a latitude", func() {
+			r := addressUs.Latitude()
+			Expect(r).To(BeNumerically(">=", -90.0))
+			Expect(r).To(BeNumerically("<=", 90.0))
 			// FIXME: testutil.GetDecimalLength is not accurate when the end of the number is 0
-			// length := testutil.GetDecimalLength(r)
-			// gt.Expect(t15, &length).ToBe(6)
+			length := testutil.GetDecimalLength(r)
+			Expect(length).To(BeNumerically("==", 6))
 		})
 	})
 
-	tLon := gt.CreateTest(testingT)
-	tLon.Describe("Longitude", func() {
-		tLon.It("should return a longitude", func() {
-			r := address.Longitude()
-
-			gt.Expect(tLon, &r).ToBe_(gt.Between(-180.0, 180.0))
+	Describe("Longitude", func() {
+		It("should return a longitude", func() {
+			r := addressUs.Longitude()
+			Expect(r).To(BeNumerically(">=", -180.0))
+			Expect(r).To(BeNumerically("<=", 180.0))
 			// FIXME: testutil.GetDecimalLength is not accurate when the end of the number is 0
-			// length := testutil.GetDecimalLength(r)
-			// gt.Expect(t16, &length).ToBe(6)
+			length := testutil.GetDecimalLength(r)
+			Expect(length).To(BeNumerically("==", 6))
 		})
 	})
 
-	tCoord := gt.CreateTest(testingT)
-	tCoord.Describe("LocalCoordinates", func() {
-		tCoord.It("should return a coordinate", func() {
-			lat, lon := address.LocalCoordinates()
-
-			gt.Expect(tCoord, &lat).ToBe_(gt.Between(-90.0, 90.0))
+	Describe("LocalCoordinates", func() {
+		It("should return a coordinate", func() {
+			lat, lon := addressUs.LocalCoordinates()
+			Expect(lat).To(BeNumerically(">=", -90.0))
+			Expect(lat).To(BeNumerically("<=", 90.0))
 			// FIXME: testutil.GetDecimalLength is not accurate when the end of the number is 0
-			// latLength := testutil.GetDecimalLength(lat)
-			// gt.Expect(t17, &latLength).ToBe(6)
+			latLength := testutil.GetDecimalLength(lat)
+			Expect(latLength).To(BeNumerically("==", 6))
 
-			gt.Expect(tCoord, &lon).ToBe_(gt.Between(-180.0, 180.0))
+			Expect(lon).To(BeNumerically(">=", -180.0))
+			Expect(lon).To(BeNumerically("<=", 180.0))
 			// FIXME: testutil.GetDecimalLength is not accurate when the end of the number is 0
-			// lonLength := testutil.GetDecimalLength(lon)
-			// gt.Expect(t17, &lonLength).ToBe(6)
-
+			lonLength := testutil.GetDecimalLength(lon)
+			Expect(lonLength).To(BeNumerically("==", 6))
 		})
 	})
 
 	localizedJaJp := ja_JP.New()
-	addressJaJp := New(coreRand, localizedJaJp)
+	addressJaJp := address.New(coreRand, localizedJaJp)
 
-	tPref := gt.CreateTest(testingT)
-	tPref.Describe("Prefecture", func() {
-		tPref.It("should return a prefecture", func() {
+	Describe("Prefecture", func() {
+		It("should return a prefecture", func() {
 			r := addressJaJp.Prefecture()
-			gt.Expect(tPref, &r).ToBeIn(ja_JP.Prefectures)
+			Expect(r).To(BeElementOf(ja_JP.Prefectures))
 		})
 	})
 
-	tWard := gt.CreateTest(testingT)
-	tWard.Describe("Ward", func() {
-		tWard.Test("WardSuffix should return a ward suffix", func() {
+	Describe("Ward", func() {
+		It("WardSuffix should return a ward suffix", func() {
 			r := addressJaJp.WardSuffix()
-			gt.Expect(tWard, &r).ToBeIn(ja_JP.WardSuffixes)
+			Expect(r).To(BeElementOf(ja_JP.WardSuffixes))
 		})
 
-		tWard.Test("WardName should return a ward name", func() {
+		It("WardName should return a ward name", func() {
 			r := addressJaJp.WardName()
-			gt.Expect(tWard, &r).ToBeIn(ja_JP.WardNames)
+			Expect(r).To(BeElementOf(ja_JP.WardNames))
 		})
 
-		tWard.Test("Ward should return a ward", func() {
+		It("Ward should return a ward", func() {
 			r := addressJaJp.Ward()
 			testutil.Output("Address.Ward", r)
 		})
 	})
 
-	tArea := gt.CreateTest(testingT)
-	tArea.Describe("Area", func() {
-		tArea.Test("AreaNumber should return an area number", func() {
+	Describe("Area", func() {
+		It("AreaNumber should return an area number", func() {
 			r := addressJaJp.AreaNumber()
 			testutil.Output("Address.AreaNumber", r)
 		})
 
-		tArea.Test("AreaName should return an area name", func() {
+		It("AreaName should return an area name", func() {
 			r := addressJaJp.AreaName()
-			gt.Expect(tArea, &r).ToBeIn(ja_JP.AreaNames)
+			Expect(r).To(BeElementOf(ja_JP.AreaNames))
 		})
 
-		tArea.Test("Area should return an area", func() {
+		It("Area should return an area", func() {
 			r := addressJaJp.Area()
 			testutil.Output("Address.Area", r)
 		})
 	})
 
-	tSecJaJp := gt.CreateTest(testingT)
-	tSecJaJp.Describe("SecondaryAddress for ja_JP", func() {
-		tSecJaJp.Test("BuildingName should return a building name", func() {
+	Describe("SecondaryAddress for ja_JP", func() {
+		It("BuildingName should return a building name", func() {
 			r := addressJaJp.BuildingName()
-			gt.Expect(tSecJaJp, &r).ToBeIn(*ja_JP.BuildingNames)
+			Expect(r).To(BeElementOf(ja_JP.BuildingNames))
 		})
-		tSecJaJp.Test("RoomNumber should return a room number", func() {
+
+		It("RoomNumber should return a room number", func() {
 			r := addressJaJp.RoomNumber()
 			testutil.Output("Address.RoomNumber", r)
 		})
-		tSecJaJp.Test("SecondaryAddress for ja_JP should return a secondary address", func() {
+
+		It("SecondaryAddress for ja_JP should return a secondary address", func() {
 			r := addressJaJp.SecondaryAddress()
 			testutil.Output("Address.SecondaryAddress", r)
 		})
 	})
 
-	tAddrJaJp := gt.CreateTest(testingT)
-	tAddrJaJp.Describe("Address for ja_JP", func() {
-		tAddrJaJp.It("should return an address", func() {
+	Describe("Address for ja_JP", func() {
+		It("should return an address", func() {
 			r := addressJaJp.Address()
 			testutil.Output("Address.Address", r)
 		})
 	})
-}
+
+})
