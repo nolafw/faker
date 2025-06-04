39ecfb3c4382a2f4853e0af5b728119fcfcc8212
ichikawa <68413973+yrichika@users.noreply.github.com>
2025-06-04 17:55:36 +0900

    Refactor file tests: migrate to Ginkgo and update test structure for improved readability

--- pkg/fk/generator/file/file_test.go
+++ pkg/fk/generator/file/file_test.go
@@ -1,108 +1,105 @@
-package file
+package file_test
 
 import (
 	"os"
 	"path/filepath"
-	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 
 	"github.com/nolafw/faker/pkg/fk/common/util"
 	"github.com/nolafw/faker/pkg/fk/core"
+	"github.com/nolafw/faker/pkg/fk/generator/file"
 	"github.com/nolafw/faker/pkg/fk/provider"
 	"github.com/nolafw/faker/pkg/fk/provider/global"
-	"github.com/yrichika/gest/pkg/gt"
 )
 
-func TestFile(testingT *testing.T) {
+var _ = Describe("File", Ordered, func() {
 	coreRand := core.NewRand(util.RandSeed())
 	global := &provider.Global{
 		Files: global.CreateFiles(),
 	}
 
-	file := New(coreRand, global)
+	f := file.New(coreRand, global)
 
-	t := gt.CreateTest(testingT)
-	t.Describe("File", func() {
-		t.Test("MimeType should return a string", func() {
-			mimeType := file.MimeType()
-
+	Describe("File", func() {
+		It("MimeType should return a string", func() {
+			mimeType := f.MimeType()
 			_, exists := global.Files.MimeTypes[mimeType]
-			gt.Expect(t, &exists).ToBe(true)
+			Expect(exists).To(BeTrue())
 		})
 
-		t.Test("Extension should return a string", func() {
-			extension := file.Extension()
-
-			gt.Expect(t, &extension).ToMatchRegex(`^[0-9a-z-]{2,20}$`)
+		It("Extension should return a string", func() {
+			extension := f.Extension()
+			Expect(extension).To(MatchRegexp(`^[0-9a-z-]{2,20}$`))
 		})
 	})
-
-	t2 := gt.CreateTest(testingT)
 	// NOTICE: you could use `t.TempDir() to create a temporary directory.
 	// But sometimes I want to see what's in the directory for debugging.
 	// So I'm using a fixed directory here.
 	destDir := "../../../../tmp"
-	t2.AfterAll(func() {
+
+	Describe("File", func() {
+		content := "Hello, World!"
+		extension := "txt"
+
+		When("returnFullPath false", func() {
+			returnFullPath := false
+			It("WriteWithText should create file and return a relative path", func() {
+				filePath, err := f.WriteWithText(destDir, content, extension, returnFullPath)
+				r := FileExists(filePath)
+				Expect(r).To(BeTrue())
+				Expect(filePath).To(MatchRegexp(`^..\/..\/..\/..\/tmp/[\d\w]{16}\.txt$`))
+				Expect(err).To(BeNil())
+			})
+		})
+
+		When("returnFullPath true", func() {
+			returnFullPath := true
+			It("WriteWithText should return absolute path", func() {
+				filePath, err := f.WriteWithText(destDir, content, extension, returnFullPath)
+				r := FileExists(filePath)
+				Expect(r).To(BeTrue())
+				absPath, _ := filepath.Abs(filePath)
+				Expect(filePath).To(Equal(absPath))
+				Expect(err).To(BeNil())
+			})
+		})
+	})
+
+	Describe("CopyFrom", func() {
+		srcFilePath := "../../../../testdata/files/sample.txt"
+		extension := "txt"
+
+		When("returnFullPath is false", func() {
+			returnFullPath := false
+			It("CopyFrom should copy file from src to dest and should return relative path", func() {
+				filePath, err := f.CopyFrom(destDir, srcFilePath, extension, returnFullPath)
+				r := FileExists(filePath)
+				Expect(r).To(BeTrue())
+				Expect(filePath).To(MatchRegexp(`^..\/..\/..\/..\/tmp/[\d\w]{16}\.txt$`))
+				Expect(err).To(BeNil())
+			})
+		})
+
+		When("returnFullPath is true", func() {
+			returnFullPath := true
+			It("CopyFrom should copy file from src to dest and should return full path", func() {
+				filePath, err := f.CopyFrom(destDir, srcFilePath, extension, returnFullPath)
+				r := FileExists(filePath)
+				Expect(r).To(BeTrue())
+				absPath, _ := filepath.Abs(filePath)
+				Expect(filePath).To(Equal(absPath))
+				Expect(err).To(BeNil())
+			})
+		})
+	})
+
+	AfterAll(func() {
 		// Clean up
 		deleteFilesInDir(destDir)
 	})
-	t2.Describe("File", func() {
-		content := "Hello, World!"
-		extension := "txt"
-		t2.Test("WriteWithText should create file and return a relative path if returnFullPath false", func() {
-			returnFullPath := false
-			filePath, err := file.WriteWithText(destDir, content, extension, returnFullPath)
-
-			r := FileExists(filePath)
-			gt.Expect(t2, &r).ToBe(true)
-			gt.Expect(t2, &filePath).ToMatchRegex(`^..\/..\/..\/..\/tmp/[\d\w]{16}\.txt$`)
-
-			gt.Expect(t2, &err).ToBeNilInterface()
-
-		})
-
-		t2.Test("WriteWithText should return absolute path if returnFullPath true", func() {
-			returnFullPath := true
-			filePath, err := file.WriteWithText(destDir, content, extension, returnFullPath)
-
-			r := FileExists(filePath)
-			gt.Expect(t2, &r).ToBe(true)
-
-			absPath, _ := filepath.Abs(filePath)
-			gt.Expect(t2, &filePath).ToBe(absPath)
-
-			gt.Expect(t2, &err).ToBeNilInterface()
-		})
-
-		srcFilePath := "../../../../testdata/files/sample.txt"
-		t2.Test("CopyFrom should copy file from src to dest and should return relative path if returnFullPath is false", func() {
-			returnFullPath := false
-			extension := "txt"
-			filePath, err := file.CopyFrom(destDir, srcFilePath, extension, returnFullPath)
-
-			r := FileExists(filePath)
-			gt.Expect(t2, &r).ToBe(true)
-			gt.Expect(t2, &filePath).ToMatchRegex(`^..\/..\/..\/..\/tmp/[\d\w]{16}\.txt$`)
-
-			gt.Expect(t2, &err).ToBeNilInterface()
-
-		})
-
-		t2.Test("CopyFrom should copy file from src to dest and should return fill path if returnFullPath is true", func() {
-			returnFullPath := true
-			extension := "txt"
-			filePath, err := file.CopyFrom(destDir, srcFilePath, extension, returnFullPath)
-
-			r := FileExists(filePath)
-			gt.Expect(t2, &r).ToBe(true)
-
-			absPath, _ := filepath.Abs(filePath)
-			gt.Expect(t2, &filePath).ToBe(absPath)
-
-			gt.Expect(t2, &err).ToBeNilInterface()
-
-		})
-	})
-}
+})
 
 func FileExists(filePath string) bool {
 	_, pathErr := os.Stat(filePath)
